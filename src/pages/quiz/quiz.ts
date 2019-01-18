55e51e932d28b57befab376752bad64c1e5082a0
araujo21x <araujolucas97@outlook.com>
2019-01-18 16:48:12 -0300

    Dia 18/01

--- src/pages/quiz/quiz.ts
+++ src/pages/quiz/quiz.ts
@@ -1,58 +1,53 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { LoginServiceProvider } from '../../providers/login-service/login-service';
-import { QuizServiceProvider } from '../../providers/quiz-service/quiz-service';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Component } from "@angular/core";
+import { IonicPage, NavController, NavParams } from "ionic-angular";
+import { LoginServiceProvider } from "../../providers/login-service/login-service";
+import { QuizServiceProvider } from "../../providers/quiz-service/quiz-service";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 
 @IonicPage()
 @Component({
-  selector: 'page-quiz',
-  templateUrl: 'quiz.html',
+  selector: "page-quiz",
+  templateUrl: "quiz.html"
 })
 export class QuizPage {
-
   private question: any = {};
   private quizForm: FormGroup;
 
-  constructor(public navCtrl: NavController,
+  constructor(
+    public navCtrl: NavController,
     public navParams: NavParams,
     public formBuilder: FormBuilder,
     public loginServiceProvider: LoginServiceProvider,
     public quizServiceProvider: QuizServiceProvider
-    ) {
-      this.quizForm = formBuilder.group({
-        onequestion: ['', [Validators.required]], 
-        twoquestion: ['', [Validators.required]],
-        threequestion: ['', [Validators.required]], 
-        fourquestion: ['', [Validators.required]],
-        fivequestion: ['', [Validators.required]], 
-        sixquestion: ['', [Validators.required]],
-        sevenquestion: ['', [Validators.required]], 
-        eightquestion: ['', [Validators.required]]
-      });
-  }
-
-  public logout(){
-    this.loginServiceProvider.logout().then( success => {
-      this.navCtrl.setRoot("LoginPage");
-    }).catch(error => {
-      let message:string = error.message;
-      this.loginServiceProvider.toastCall(message);
+  ) {
+    this.quizForm = formBuilder.group({
+      onequestion: ["", [Validators.required]],
+      twoquestion: ["", [Validators.required]],
+      threequestion: ["", [Validators.required]],
+      fourquestion: ["", [Validators.required]],
+      fivequestion: [, [Validators.required]],
+      sixquestion: ["", [Validators.required]],
+      sevenquestion: ["", [Validators.required]],
+      eightquestion: ["", [Validators.required]]
     });
   }
 
-  public saveQuiz(){
-
-    //const formValue = this.quizForm.value;
-    
-    //if (!formValue){
-    //  console.log("erro")
-    //} else {
-      this.quizServiceProvider.save(this.question);
-      this.loginServiceProvider.toastCall("Dados Salvos Com Sucesso");
-      this.quizForm.reset();
-    //}
-
+  public logout() {
+    this.loginServiceProvider
+      .logout()
+      .then(success => {
+        this.navCtrl.setRoot("LoginPage");
+      })
+      .catch(error => {
+        let message: string = error.message;
+        this.loginServiceProvider.toastCall(message, "top");
+      });
   }
 
+  public saveQuiz() {
+
+    this.quizServiceProvider.save(this.question);
+    this.loginServiceProvider.toastCall("Dados Salvos Com Sucesso", "middle");
+    this.quizForm.reset();
+  }
 }
